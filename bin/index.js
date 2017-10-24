6bd338da4596cd34749a18a8d2cbf04d8896af2a
vytch <vytch1906@gmail.com>
2017-10-24 14:21:49 +1100

    Supporting alternate config file.

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,24 @@
 
 var shell = require("shelljs");
 var path = require("path");
+var argv = require('minimist')(process.argv.slice(2));
+// config
 
-var configPath = path.join(__dirname, '..', 'lib','run.js');
-shell.exec("casperjs test " + configPath);
+var cmdArray = [];
+
+var modulepath = path.join(__dirname, '..', 'lib');
+var configPath = path.join(modulepath,'run.js');
+
+// Building the command line based on what areguments we have.
+cmdArray.push("casperjs test");
+cmdArray.push(configPath);
+cmdArray.push("--path=" + modulepath);
+
+// If we find any config paramater, we port it to the casperjs command.
+if(typeof argv['config'] != 'undefined'){
+    cmdArray.push("--c=" + argv['config']);
+} else {
+    console.dir(argv);
+}
+
+shell.exec(cmdArray.join(' '));
